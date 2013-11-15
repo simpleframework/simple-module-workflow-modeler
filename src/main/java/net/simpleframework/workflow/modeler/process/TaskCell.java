5b667f1d9aa354a988735ff47c29960efc6ce937
simpleframework <cknet@126.com>
2013-11-15 12:17:26 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/workflow/modeler/process/TaskCell.java
+++ src/main/java/net/simpleframework/workflow/modeler/process/TaskCell.java
@@ -13,8 +13,7 @@ import com.mxgraph.model.mxGeometry;
 /**
  * Licensed under the Apache License, Version 2.0
  * 
- * @author 陈侃(cknet@126.com, 13910090885)
- *         https://github.com/simpleframework
+ * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public class TaskCell extends mxCell {
--- src/main/java/net/simpleframework/workflow/modeler/utils/JTabbedPaneEx.java
+++ src/main/java/net/simpleframework/workflow/modeler/utils/JTabbedPaneEx.java
@@ -26,8 +26,7 @@ import javax.swing.plaf.metal.MetalTabbedPaneUI;
 /**
  * Licensed under the Apache License, Version 2.0
  * 
- * @author 陈侃(cknet@126.com, 13910090885)
- *         https://github.com/simpleframework
+ * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 @SuppressWarnings("serial")
