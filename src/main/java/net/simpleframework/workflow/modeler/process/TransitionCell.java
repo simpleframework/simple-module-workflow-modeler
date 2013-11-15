40c8055c68152a9851d6a1ea15cd65145713cc9b
simpleframework <cknet@126.com>
2013-11-15 11:42:58 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/workflow/modeler/process/TransitionCell.java
+++ src/main/java/net/simpleframework/workflow/modeler/process/TransitionCell.java
@@ -12,7 +12,7 @@ import com.mxgraph.util.mxPoint;
  * Licensed under the Apache License, Version 2.0
  * 
  * @author 陈侃(cknet@126.com, 13910090885)
- *         http://code.google.com/p/simpleframework/
+ *         https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public class TransitionCell extends mxCell {
